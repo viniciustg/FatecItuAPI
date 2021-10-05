6838778297b2b127ff7502c838dec227e056d5a1
User <User@DESKTOP-27FEHD8>
2021-10-05 17:18:34 -0300

    Adicionar arquivos de projeto.

---
+++ src/FatecItuAPI/FatecItuApi/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FatecItuApi.Api.ViewModels;
+using FatecItuApi.Business.Models;
+using FatecItuApi.ViewModels;
+
+namespace FatecItuApi.Configuration
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
---
+++ src/FatecItuAPI/FatecItuApi/Controllers/MainController.cs
@@ -0,0 +1,35 @@
+﻿using FatecItuApi.Api.ViewModels;
+using FatecItuApi.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FatecItuApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MainController : ControllerBase
+    {
+        //validacao de notificacoes de erro
+
+        //validacao de model state
+
+        //validacao da operacao de negocios
+    }
+
+    public class FornecedoresController : MainController
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public FornecedoresController(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task<ActionResult<IEnumerable<ProdutoViewModel>>> ObterTodos()
+        {
+            var fornecedores = await _produtoRepository.ObterTodos();
+            return Ok(fornecedores);
+        }
+    }
+}
